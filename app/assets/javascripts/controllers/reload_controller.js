2d2073284206a938c6e0379289f764608a280a33
Yasuhiro Usutani <y_usutani@mac.com>
2021-07-31 17:14:50 +0900

    Update status

---
+++ app/assets/javascripts/controllers/reload_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static values = { url: String }
+
+  initialize() {
+    this.intervalId = 0
+  }
+
+  connect() {
+    this.intervalId = setInterval(() => {
+      if (this.canReload()) {
+        this.fetchStream()
+      } else {
+        this.stopReloading()
+      }
+    }, 1000);
+  }
+
+  fetchStream() {
+    fetch(this.urlValue, { headers: { 'Accept': 'text/vnd.turbo-stream.html' } })
+      .then(response => response.text())
+      .then(message => Turbo.renderStreamMessage(message))
+      .catch (() => this.stopReloading())
+  }
+
+  canReload() {
+    const reload = document.getElementById('reload').textContent
+    return (reload === 'true')
+  }
+
+  stopReloading() {
+    if (this.intervalId !== 0) {
+      clearInterval(this.intervalId)
+      this.intervalId = 0
+    }
+  }
+}
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
+  RELOAD_MAX = 3
+
   def index
+    @@reload_count = 1
+  end
+
+  def status
+    @textContent = "#{@@reload_count}/#{RELOAD_MAX}, #{Time.now}, "
+
+    @@reload_count += 1
+    @reload = (@@reload_count <= RELOAD_MAX)
   end
 end
